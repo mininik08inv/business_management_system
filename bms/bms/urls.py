899fd80facd936b6b03f4f55b557e82d822671a9
mininik08inv <mininik08inv@yandex.ru>
2025-05-27 10:24:57 +0300

    url

--- bms/bms/urls.py
+++ bms/bms/urls.py
@@ -8,5 +8,6 @@ logger = logging.getLogger(__name__)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users')),
+    path('tasks/', include('tasks.urls', namespace='tasks')),
     path('', include('core.urls', namespace='core')),
 ]
